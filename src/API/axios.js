87a35e71705c9c7faffde4fde05a6d7bea64157b
Fedlu AWEL <anisajemila84@gmail.com>
2025-03-18 05:52:35 -0600

    commit

--- src/API/axios.js
+++ src/API/axios.js
@@ -5,9 +5,7 @@ const axiosinstance = axios.create({
   //baseURL: "http://127.0.0.1:5001/clone-499ea/us-central1/api",
 
   // Deployed baseURL deployed on render
-  baseURL: "https://amazon-api-deploy-1-5ceu.onrender.com/",
+  baseURL: "http://localhost:5000/",
 });
 
-
-
-export  {axiosinstance}
+export { axiosinstance };
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import DataProvider from "./Components/DataProvider/DataProvider.jsx";
+import DataProvider from "./components/DataProvider/DataProvider.jsx";
 import {initialState,reducer} from './Utility/reducer.js'
 
 createRoot(document.getElementById("root")).render(
