d96bea53728779c75c731c9d03d5d3d4c69ba3d3
Fedlu AWEL <anisajemila84@gmail.com>
2025-03-18 06:13:46 -0600

    commit

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const axiosinstance = axios.create({
   //local baseURL
-  //baseURL: "http://127.0.0.1:5001/clone-499ea/us-central1/api",
+  baseURL: "https://amazon-backend-qa1u.onrender.com/",
 
   // Deployed baseURL deployed on render
-  baseURL: "http://localhost:5000/",
+  // baseURL: "http://localhost:5000/",
 });
 
 export { axiosinstance };
